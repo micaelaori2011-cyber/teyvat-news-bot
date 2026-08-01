f780fb08fd1cb7a7b261ccc681002baf73432993
micaelaori2011-cyber <micaelaori2011@gmail.com>
2026-08-01 19:43:59 -0300

    Update noticias.py

--- noticias.py
+++ noticias.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+
 URL_NOTICIAS = "https://www.hoyolab.com/circles/2/27/official"
+
 # Aquí guardaremos el sistema de noticias de Teyvat News
 def revisar_noticias():
     respuesta = requests.get(URL_NOTICIAS)
+
     if respuesta.status_code == 200:
-        return respuesta.text[:500]
-    else:
-        return "❌ No se pudo conectar con HoYoverse."
+        pagina = BeautifulSoup(respuesta.text, "html.parser")
+        return pagina.title.text
+
+    return "❌ No se pudo conectar con HoYoverse."

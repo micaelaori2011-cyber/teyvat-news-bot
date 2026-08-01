bad05dafde38a59c34cac668776f347def9d4c54
micaelaori2011-cyber <micaelaori2011@gmail.com>
2026-07-31 21:00:24 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.guilds = True
+intents.messages = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ {bot.user} está conectado.")

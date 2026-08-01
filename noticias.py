96f9debc300a98c459932bd8ea93b9596a0b8d4f
micaelaori2011-cyber <micaelaori2011@gmail.com>
2026-08-01 20:06:01 -0300

    Update noticias.py

--- noticias.py
+++ noticias.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL_NOTICIAS = "https://rsshub.app/hoyolab/news/es-mx/2/1"
+URL_NOTICIAS = "https://bbs-api.hoyolab.com/community/post/wapi/getNewsList?gids=2&type=1&page_size=1"
 
 # Aquí guardaremos el sistema de noticias de Teyvat News
 def revisar_noticias():
     respuesta = requests.get(URL_NOTICIAS)
 
     if respuesta.status_code == 200:
-        pagina = BeautifulSoup(respuesta.text, "html.parser")
-        return pagina.title.text
+        datos = respuesta.json()
+        return str(datos)
 
     return "❌ No se pudo conectar con HoYoverse."

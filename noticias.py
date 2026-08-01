4c380bc8f4b4802387993ded2b1634437a5b45bd
micaelaori2011-cyber <micaelaori2011@gmail.com>
2026-08-01 20:09:33 -0300

    Update noticias.py

--- noticias.py
+++ noticias.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL_NOTICIAS = "https://bbs-api.hoyolab.com/community/post/wapi/getNewsList?gids=2&type=1&page_size=1"
+URL_NOTICIAS = "https://bbs-api-os.hoyolab.com/community/post/wapi/getNewsList?gids=2&type=1&page_size=1"
 
 # Aquí guardaremos el sistema de noticias de Teyvat News
 def revisar_noticias():

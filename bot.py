07f3489b40179e6a6ed0f1909cf530306751e429
micaelaori2011-cyber <micaelaori2011@gmail.com>
2026-08-01 16:42:09 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ def home():
     return "Bot funcionando"
 
 def run():
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
 Thread(target=run).start()
 

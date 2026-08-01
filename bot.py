34e7443a5fea019bb3b7d1af19e670b15cdf8519
micaelaori2011-cyber <micaelaori2011@gmail.com>
2026-07-31 21:21:36 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,16 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.guilds = True
-intents.messages = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-
 @bot.event
 async def on_ready():
-    print(f"✅ {bot.user} está conectado.")
+    print(f"✅ {bot.user} está conectado")
+    import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+bot.run(TOKEN)

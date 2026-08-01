7ab7af6a1b93b9ff7f5ec24addc841d35defd986
micaelaori2011-cyber <micaelaori2011@gmail.com>
2026-07-31 22:23:32 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.guilds = True
@@ -8,11 +13,10 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
+    canal = discord.utils.get(bot.get_all_channels(), name="paimon")
+    if canal:
+        await canal.send("🌸 Teyvat News está conectado correctamente.")
     print(f"✅ {bot.user} está conectado")
-    import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-TOKEN = os.getenv("DISCORD_TOKEN")
 bot.run(TOKEN)
+

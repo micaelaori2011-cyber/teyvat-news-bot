30484ec1ab3b1430afc0f6915200ed2d941502aa
micaelaori2011-cyber <micaelaori2011@gmail.com>
2026-08-01 21:45:59 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,21 +25,28 @@ intents.guilds = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+
 @tasks.loop(minutes=5)
 async def revisar_noticias():
     canal = discord.utils.get(bot.get_all_channels(), name="𓏼﹒paimon﹒⭐")
+
     if canal:
-        await canal.send(obtener_noticias())
-        
+        noticia = obtener_noticias()
+
+        if noticia != "No hay noticias nuevas.":
+            await canal.send(noticia)
+
+
 @bot.event
 async def on_ready():
     revisar_noticias.start()
-    canal = discord.utils.get(bot.get_all_channels(), name="𓏼﹒paimon﹒⭐")
-    if canal:
-        await canal.send("🌸 Teyvat News está conectado correctamente.")
     print(f"✅ {bot.user} está conectado")
+
+
 @bot.command()
 async def ping(ctx):
     await ctx.send("🏓 ¡Pong! Teyvat News está funcionando.")
-bot.run(TOKEN)
 
+
+bot.run(TOKEN)
